5a9b0d052adaaaf4812945252232c4cf76d79d59
R0gacz <62551410+R0gacz@users.noreply.github.com>
2021-11-30 12:18:45 +0100

    Add files via upload

--- Magetometria/Tb_t.py
+++ Magetometria/Tb_t.py
@@ -14,6 +14,28 @@ sH_MT = MT_Tb["sH"] # sH - niepewność mi0HmT.
 M_emu_MT = MT_Tb["Memu"] # Memu - namagnesowanie w emu. 
 sM_emu__MT = MT_Tb["sMemu"] # sMemu - niepewność namagnesowania.
 
+data_new = MT_Tb["TrealK"][MT_Tb["TrealK"]>240]
+print(data_new)
+y_new = (HmT_mi0_MT/M_emu_MT)[195:]
+
+
+
+def func(x, a, b):
+    return a*x + b
+
+popt2, pcov2 = curve_fit(func, data_new, y_new)
+
+x_fit = np.linspace(240,293,1000)
+
+y_fit = popt2[0]*x_fit + popt2[1]
+
 plt.figure(dpi = 200)
-plt.plot(T_realK_MT, 1/M_emu_MT, ".")
-plt.show()
+plt.plot(data_new, y_new, ".", ms = 0.5)
+plt.plot(x_fit,y_fit, "--", lw = 0.5)
+plt.xlabel("Temperatura T[K]")
+plt.grid()
+plt.ylabel("Odwrotnośc podatności magnetycznej []")
+plt.savefig("plots/podatnosc.png")
+
+print(popt2[1])
+print(pcov2[0][0])
